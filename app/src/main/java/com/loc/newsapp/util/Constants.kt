f2362ceebec84180852479db2d9be5f12e626a7b
Meliani <meliani@Melianis-MacBook-Air.local>
2024-07-22 03:11:22 +0700

    first commit

---
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.loc.newsapp.util
+
+object Constants {
+
+    const val USER_SETTINGS = "user_settings"
+    const val APP_ENTRY = "app_entry"
+
+    const val BASE_URL = "https://newsapi.org/v2/"
+    const val API_KEY = "a5a100a63bb545988ffea8e885a04186"
+
+}
